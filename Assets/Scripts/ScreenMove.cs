52c104563d2115f8e896c320819662e4d58f0b22
Alex Kearns <43974693+Kearns-Alex@users.noreply.github.com>
2020-10-17 01:31:58 -0400

    Map Area & Movement
    Completed the Overworld layout and added in triggers to handel moving the camera from screen to screen.
    Animated the waterfall and added the ability to "have a secret there" in the future.
    Added template for "triggering messages on screen (dungeon entered??) -this is disabled but organized in the tree.

---
+++ Assets/Scripts/ScreenMove.cs
@@ -0,0 +1,89 @@
+﻿#define LOGS
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenMove : MonoBehaviour
+{
+	// the different directions that we can move from screen to screen
+	public enum Direction : byte
+	{
+		 NORTH_SOUTH
+		,EAST_WEST
+	}
+	
+	public Direction direction = Direction.NORTH_SOUTH;
+	public Vector2 cameraChange = new Vector2(16, 11);
+
+	private CameraMovement cam;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// set the camera object
+		cam = Camera.main.GetComponent<CameraMovement>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Vector2 _cameraChange = cameraChange;
+
+		// check to see that the player tag is attached to the one colliding
+		if (!other.CompareTag("Player"))
+		{
+			return;
+		}
+
+		// see what direction we are moving and make changes 
+		switch(direction)
+		{
+			case Direction.NORTH_SOUTH:
+				_cameraChange.x = 0;
+
+				// check if we are coming from the North
+				if (transform.GetComponent<Collider2D>().bounds.center.y < other.bounds.center.y)
+				{
+					_cameraChange.y *= -1;
+#if (LOGS)
+					print("Move South");
+				}
+				else
+				{
+					print("Move North");
+#endif
+				}
+				break;
+
+			case Direction.EAST_WEST:
+				_cameraChange.y = 0;
+
+				// check if we are coming from the East
+				if (transform.GetComponent<Collider2D>().bounds.center.x < other.bounds.center.x)
+				{
+					_cameraChange.x *= -1;
+#if (LOGS)
+					print("Move West");
+				}
+				else
+				{
+					print("Move East");
+#endif
+				}
+				break;
+
+			default:
+				break;
+		}
+		
+		// update the positions
+		cam.minPosition += _cameraChange;
+		cam.maxPosition += _cameraChange;
+	}
+}
