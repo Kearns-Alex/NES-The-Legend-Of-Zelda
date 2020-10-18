58d1c9dff28b0b3ba6abfdf4d519d58b88ae3ad4
Alex Kearns <43974693+Kearns-Alex@users.noreply.github.com>
2020-10-18 02:09:10 -0400

    Title Cards
    Added Title cards that can be used for Introducing an area or that you found a secret place.
    
    Added support for if it is a Text object or a TextMeshPro

---
+++ Assets/Scripts/TitleCard.cs
@@ -0,0 +1,52 @@
+﻿// if using text mesh pro, look into why text objects share the same outline settings
+#define TEXTMESH_PRO
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if TEXTMESH_PRO
+using TMPro;
+#else
+using UnityEngine.UI;
+#endif
+
+
+public class TitleCard : MonoBehaviour
+{
+    public string placeName;
+    public GameObject text;
+#if TEXTMESH_PRO
+    public TextMeshProUGUI placeText;
+#else
+    public Text placeText;
+#endif
+
+    public float displayTime = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(PlaceNameCo());
+    }
+
+    // method that can run paralel to other processes and allows a specified wait time
+    private IEnumerator PlaceNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(displayTime);
+        text.SetActive(false);
+    }
+}
